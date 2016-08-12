552c11fe45c09b42713ba1cf083124d733542eaf
zhao.liu <zhao.liu@zhaopin.com.cn>
2016-08-12 11:39:28 +0800

    Initial Commit

---
+++ OC-Swift/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OC-Swift
+//
+//  Created by Liu Zhao on 16/8/12.
+//  Copyright © 2016年 Liu Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OC-Swift/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OC-Swift
+//
+//  Created by Liu Zhao on 16/8/12.
+//  Copyright © 2016年 Liu Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
